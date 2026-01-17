c6b73b422a29d5857acde8caed0cc475c701ee32
Atri <68886053+li7355608@users.noreply.github.com>
2026-01-17 21:33:36 +0800

    init

---
+++ bili_live_highest_quality.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         哔哩哔哩直播-自动切换最高画质
+// @description  自动切换直播间画质为最高画质。
+// @match        https://live.bilibili.com/*
+// @icon         https://www.bilibili.com/favicon.ico
+// @version      1.0
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
