834062f34984f32ca8fa3f6a69988c5e0e25d6e9
Atri <68886053+li7355608@users.noreply.github.com>
2026-01-18 14:16:34 +0800

    chore(script): 更新脚本元数据信息
    - 添加作者信息到脚本头部
    - 更新版本号从 1.4 到 1.4.1

--- bili_live_highest_quality.js
+++ bili_live_highest_quality.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         [哔哩哔哩直播]-自动切换最高画质
 // @description  自动切换直播间画质为最高画质。
+// @author       Asuna
 // @match        https://live.bilibili.com/*
 // @icon         https://www.bilibili.com/favicon.ico
-// @version      1.4
+// @version      1.4.1
 // @license      MIT
 // ==/UserScript==
 
